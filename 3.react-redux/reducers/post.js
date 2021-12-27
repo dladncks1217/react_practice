d72e2a8b940a95887c22a0fe8053dc82f6b5cfa0
dladncks1217 <dlaxodud1217@gmail.com>
2021-12-27 23:57:51 +0900

    init

--- 3.react-redux/reducers/post.js
+++ 3.react-redux/reducers/post.js
@@ -1,12 +1,19 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
 const initialState = [];
 
-const postReducer = (prevState = initialState, action) => { // 새로운 state 만들어주기
+const postReducer = (prevState = initialState, action) => {
+  // 새로운 state 만들어주기
   switch (action.type) {
-    case 'ADD_POST':
+    case "ADD_POST":
       return [...prevState, action.data];
     default:
       return prevState;
   }
 };
+
+const postSlice = createSlice({
+  name: "post",
+});
 
 module.exports = postReducer;
