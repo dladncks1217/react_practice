06ba1be12ab86cdbd6ad126058b02e527d409ab5
dladncks1217 <dlaxodud1217@gmail.com>
2022-01-16 17:15:37 +0900

    init

--- toolkit-blog/front/actions/index.js
+++ toolkit-blog/front/actions/index.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const join = createAsyncThunk("user/join", async (data, thunkAPI) => {
-  const result = await axios.post("http://localhost:8000/user/join", {
+  const result = await axios.post("http://localhost:8000/users/join", {
     name: data.name,
     email: data.email,
     password: data.password,
