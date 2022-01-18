4a1f94e8ce9072f28842eb085ae7ffa827dff70a
dladncks1217 <dlaxodud1217@gmail.com>
2022-01-18 21:23:48 +0900

    init

--- toolkit-blog/front/reducers/userSlice.js
+++ toolkit-blog/front/reducers/userSlice.js
@@ -9,22 +9,23 @@ const initialState = {
 };
 
 export const joinSlice = createSlice({
-  name: join,
+  name: "join",
   initialState,
   reducers: {},
-  extraReducers: {
-    [join.pending](state, action) {
-      state.isJoining = true;
-    },
-    [join.fulfilled](state, action) {
-      state.isJoined = true;
-      state.isJoining = false;
-      state.joinUser = action.payload.config.data;
-      state.joinRejectReason = "";
-    },
-    [join.rejected](state, action) {
-      state.isJoining = false;
-      state.joinErrorReasion = action.error;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(join.pending, (state, action) => {
+        state.isJoining = true;
+      })
+      .addCase(join.fulfilled, (state, action) => {
+        state.isJoined = true;
+        state.isJoining = false;
+        state.joinUser = action.payload.config.data;
+        state.joinRejectReason = "";
+      })
+      .addCase(join.rejected, (state, action) => {
+        state.isJoining = false;
+        state.joinErrorReasion = action.error;
+      });
   },
 });
