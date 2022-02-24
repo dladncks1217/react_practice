fb96aec453749d10e4ef8e34658e9044d4f61d7b
dladncks1217 <dlaxodud1217@gmail.com>
2022-02-24 17:02:54 +0900

    init

---
+++ nextex1/components/AuctionCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Card, Avatar } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+
+const { Meta } = Card;
+
+const AutionCard = () => {
+  return (
+    <Card
+      hoverable
+      style={{ width: 240, display: "inline-block", margin: 40 }}
+      cover={
+        <img
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      }
+    >
+      <Meta title="Europe Street beat" description="www.instagram.com" />
+    </Card>
+  );
+};
+
+export default AutionCard;
--- nextex1/pages/index.js
+++ nextex1/pages/index.js
@@ -1,7 +1,24 @@
+import { List } from "antd";
 import React from "react";
+import AutionCard from "../components/AuctionCard";
+import AuctionList from "../components/AutionList";
 
 const Index = () => {
-  return <div>adsf</div>;
+  return (
+    <>
+      <br />
+      <List header="경매 예정 목록">
+        <AutionCard />
+        <AutionCard />
+        <AutionCard />
+        <AutionCard />
+        <AutionCard />
+      </List>
+      <List header="경매 진행 목록">
+        <AuctionList />
+      </List>
+    </>
+  );
 };
 
 export default Index;
