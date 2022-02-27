ee78e36a768b1e5e4d823695d79e315d804eaa30
dladncks1217 <dlaxodud1217@gmail.com>
2022-02-27 21:37:40 +0900

    init

---
+++ csscheck/pages/_app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../public/App.css";
+
+const Index = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default Index;
---
+++ csscheck/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <>
+      <div id="div">나는 div</div>
+      {/* <div>나는 div</div> */}
+    </>
+  );
+};
+
+export default Index;
