615601677b195732462a317ea58134a64693a824
dladncks1217 <dlaxodud1217@gmail.com>
2022-03-02 16:56:26 +0900

    init

--- csscheck/pages/index.js
+++ csscheck/pages/index.js
@@ -1,59 +1,52 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 
 const Index = () => {
+  const date = new Date();
+  const [clicked, onChangeClicked] = useState(0);
+  const [id, onChangeId] = useState("");
+  const [password, onChangePassword] = useState("");
+  const [loggedIn, onChangeLoggedIn] = useState(false);
+
   const onClickButton = () => {
-    console.log("버튼이눌림");
+    onChangeClicked(++clicked);
+    console.log(
+      `${id}님이 버튼을 ${clicked}번째 눌렀습니다. 시간:${date.getHours()}시${date.getMinutes()}분${date.getSeconds()}초`
+    );
   };
+
+  const handleId = useCallback((e) => {
+    onChangeId(e.target.value);
+  });
+
+  const handlePassword = useCallback((e) => {
+    onChangePassword(e.target.value);
+  });
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      onChangeLoggedIn(true);
+      console.log(
+        `${id}님이 로그인했습니다. 시간:${date.getHours()}시${date.getMinutes()}분${date.getSeconds()}초`
+      );
+    },
+    [id, password]
+  );
   return (
     <>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <div id="div"></div>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-      <p>asdfasdfsad</p>
-
-      {/* <button id="btn" onClick={onClickButton}>
-        나는야버튼
-      </button> */}
-      {/* <div>나는 div</div> */}
+      {loggedIn ? (
+        <button id="countbtn" onClick={onClickButton}>
+          클릭
+        </button>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input type="text" id="id" onChange={handleId} />
+          <input type="password" id="password" onChange={handlePassword} />
+          <button id="submit" type="submit">
+            로그인
+          </button>
+        </form>
+      )}
     </>
   );
 };
